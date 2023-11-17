15424a05c6ed910d6003f9cbf83d485e90ad6851
Ben Sampica <benjamin.sampica@gmail.com>
2023-11-16 21:13:53 -0600

    Add v1.0

---
+++ broadcast-image/broadcast-image.js
@@ -0,0 +1,17 @@
+async function onBroadcastImageAsync()
+{
+    document.getElementById("invalid-state").classList.add("d-none");
+
+    var inputUrl = document.getElementById("input-image").value;
+    var isValidUrl = isValidUrl();
+
+    if(!isValidUrl)
+    {
+        document.getElementById("invalid-state").classList.remove("d-none");
+        return;
+    }
+
+    document.getElementById("broadcasted-image").src = inputUrl;
+
+    TS.sync();
+}
