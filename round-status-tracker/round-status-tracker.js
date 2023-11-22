5ab4be730855907dd96d73319018b1e56b27c58b
Ben Sampica <benjamin.sampica@gmail.com>
2023-11-21 22:59:16 -0600

    progress

--- round-status-tracker/round-status-tracker.js
+++ round-status-tracker/round-status-tracker.js
@@ -7,4 +7,74 @@ const buffs = [
     createBuff('Heroism', 10), createBuff('Longstrider', 10)
 ]
 
-function createBuff(name, roundDuration) { return new { name: name, roundDuration: roundDuration }}
+let trackedCreatures = [];
+let round = 0;
+let currentInitiativeIndex = 0;
+
+function createBuff(name, roundDuration) { return new { name: name, roundDuration: roundDuration } }
+
+function startTracking() {
+    round = 0;
+    trackedCreatures = TS.initiative.getQueue()
+        .filter(entry => entry.kind == "creature")
+        .map(entry => new creatureInitiativeItem(entry.id, entry.name));
+
+    if (trackedCreatures.length == 0) {
+        setInvalidState("There are no creatures in the initiative queue.");
+    }
+
+    triggerNewRound();
+}
+
+function handleInitiativeEvents(queue) {
+    // TODO: determine what type of event we got. forwards/backwards/add/remove.
+    currentInitiativeIndex = queue.activeItemIndex;
+
+    // If forward/backward, we don't need to remap creatures.
+
+    // If Add/Remove, do below.
+    let authoritativeTrackedCreatures = queue.items
+        .map(item => new creatureInitiativeItem(item.id, item.name));
+
+    let existingTrackedCreatureIds = trackedCreatures.map(ntc => ntc.id);
+    trackedCreatures = authoritativeTrackedCreatures
+        .map(atc => {
+            const isStillAlive = existingTrackedCreatureIds.includes(atc.id);
+            if (isStillAlive) {
+                const existingTrackedCreature = trackedCreatures.find(tc => tc.id == atc.id);
+                atc.buffs = existingTrackedCreature.buffs;
+                atc.round = existingTrackedCreature.round;
+                atc.conditions = existingTrackedCreature.conditions;
+                // TODO: if current initiative index matches this creatures index?
+                // TODO: increment round for this creature? (decrement buffs)
+            }
+        });
+}
+
+function refreshTrackedCreaturesDOM() {
+    document.getElementById("tracked-creature-template").content.firstElementChild.cloneNode(true);
+}
+
+function setInvalidState(message) {
+    document.getElementById("invalid-state").classList.remove("d-none");
+    document.getElementById("invalid-state-message").innerHTML = message;
+}
+
+function triggerNewRound() {
+    round++;
+    document.getElementById("round-count").innerHTML = round;
+}
+
+class creatureInitiativeItem {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+        this.buffs = [];
+        this.conditions = [];
+        this.round = 1;
+    }
+
+    incrementRound() {
+        this.round++;
+    }
+}
