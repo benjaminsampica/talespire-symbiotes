3ec95f346327c8cd6602751aa1a77854efea3f88
Ben Sampica <benjamin.sampica@gmail.com>
2023-11-25 14:57:57 -0600

    add tests

--- round-status-tracker/js/trackedCreature.test.js
+++ round-status-tracker/js/trackedCreature.test.js
@@ -12,3 +12,15 @@ test('when a round is incremented, all buffs durations are decremented', () => {
         expect(b.roundDuration).toEqual(0);
     });
 });
+
+test('when a round is decremented, all buffs durations are incremented', () => {
+    let sut = new trackedCreature(1, 'Test');
+    sut.addBuff(new buff('test1', 1));
+    sut.addBuff(new buff('test2', 1));
+
+    sut.decrementRound();
+
+    sut.buffs.forEach(b => {
+        expect(b.roundDuration).toEqual(2);
+    });
+});
