16c90e84aa7eca92ab47a72722d30c961b9366c5
Amanda Filizola <amandapaivafilizola@gmail.com>
2023-12-07 20:34:19 -0300

    Add Concentration condition
    Adding a condition ro remember that spellcaster is concentrating

--- combat-tracker/js/condition.js
+++ combat-tracker/js/condition.js
@@ -7,7 +7,7 @@ export default class Condition {
         return [
             new Condition('Blinded'), new Condition('Charmed'), new Condition('Deafened'), new Condition('Frightened'), new Condition('Grappled'),
             new Condition('Incapacitated'), new Condition('Invisible'), new Condition('Paralyzed'), new Condition('Petrified'),
-            new Condition('Poisoned'), new Condition('Prone'), new Condition('Stunned'), new Condition('Unconscious')
+            new Condition('Poisoned'), new Condition('Prone'), new Condition('Stunned'), new Condition('Unconscious'), new Condition('Concentrating')
         ];
     }
 }
